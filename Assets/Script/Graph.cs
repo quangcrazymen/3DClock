6f960209e260dfce2026a1888527025c91a451e8
quang nguyen <quangcrazymen@gmail.com>
2023-12-03 23:11:44 +0700

    +First commit

---
+++ Assets/Script/Graph.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+// using System.Numerics;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+	Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+	int resolution = 10;
+    void Awake(){
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+		var scale = Vector3.one * step;
+		for (int i = 0; i < resolution; i++) {
+			Transform point = Instantiate(pointPrefab);
+			position.x = (i + 0.5f) * step - 1f;
+			position.y = position.x * position.x;
+			point.localPosition = position;
+			point.localScale = scale;
+            point.SetParent(transform);
+		}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
